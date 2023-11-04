5d9dc59b28252dc219ab5f63bfd4496145a10433
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2023-11-04 13:53:50 -0400

    updated articlesdb.sql

--- api-pila-node/articlesdb.sql
+++ api-pila-node/articlesdb.sql
@@ -1,18 +1,17 @@
-DROP DATABASE IF EXISTS articlesdb;
-CREATE DATABASE articlesdb;
+DROP DATABASE IF EXIST  articlesdb;
+CREATE DATABASE IF NOT EXISTS articlesdb;
 
-CREATE TABLE article
-(
-    serial_no UUID PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS article (
+    serial_no UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     name      VARCHAR(255) NOT NULL,
     line      VARCHAR(255) NOT NULL,
-    brand     VARCHAR(255) NOT NULL
+    brand     VARCHAR(255) NOT NULL UNIQUE
 );
 
-INSERT INTO article (serial_no, name, line, brand)
-VALUES (gen_random_uuid(), 'Air 13', 'Macbook', 'Apple'),
-       (gen_random_uuid(), 'iPhone 14', 'Iphone', 'Apple'),
-       (gen_random_uuid(), 'Watch 2', 'Watch', 'Apple');
+INSERT INTO article (name, line, brand)
+VALUES ('Air 13', 'Macbook', 'Apple'),
+       ('iPhone 14', 'Iphone', 'Apple'),
+       ('Watch 2', 'Watch', 'Apple');
 
 
 -- SELECT * FROM article
